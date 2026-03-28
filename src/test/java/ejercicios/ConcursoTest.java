80555027ab493a9f731b5419fb6e72e4f176ba04
beron-carla <archcarlab@outlook.com.ar>
2026-03-28 17:52:47 -0300

    elimino codigo duplicado en test

--- src/test/java/ejercicios/ConcursoTest.java
+++ src/test/java/ejercicios/ConcursoTest.java
@@ -1,5 +1,6 @@
 package ejercicios;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -7,68 +8,63 @@ import java.time.LocalDateTime;
 import static ejercicios.Participante.PUNTOS_GANADOS;
 import static org.junit.jupiter.api.Assertions.*;
 public class ConcursoTest {
+    Participante maria;
+    Concurso concurso;
+    LocalDateTime fechaInicio;
+    LocalDateTime fechaFin;
+    LocalDateTime fechaInscripcion;
 
+    @BeforeEach
+    void setUp(){
+        this.maria = new Participante(1, "maria", "23456789", "mperez@gmail.com");
+        this.fechaInicio = LocalDateTime.of(2025, 2, 11, 12, 10, 5);
+        this.fechaFin = LocalDateTime.of(2025, 2, 15, 12, 20, 1);
+        this.concurso = new Concurso(1, "concurso 1", fechaInicio, fechaFin);
+    }
     @Test
     public void testInscripciónExitosa() {
         //un participante se inscribe a un concurso
         //setup
-        var maria = new Participante(1, "maria", "23456789", "mperez@gmail.com");
-        LocalDateTime fechaInicio = LocalDateTime.of(2025, 2, 11, 12, 10, 5);
-        LocalDateTime fechaFin = LocalDateTime.of(2025, 2, 15, 12, 20, 1);
-        LocalDateTime fechaInscripcion = LocalDateTime.of(2025, 2, 13, 12, 20, 1);
-        Concurso unConcurso = new Concurso(1, "concurso 1", fechaInicio, fechaFin);
+        this.fechaInscripcion = LocalDateTime.of(2025, 2, 13, 12, 20, 1);
 
         //ejercitación
-        unConcurso.nuevaInscripcion(maria, fechaInscripcion);
+        concurso.nuevaInscripcion(maria, fechaInscripcion);
 
         //verificación
-        assertTrue(unConcurso.participanteInscripto(maria));
-        assertEquals(1, unConcurso.cantidadInscriptos());
+        assertTrue(concurso.participanteInscripto(maria));
+        assertEquals(1, concurso.cantidadInscriptos());
     }
 
     @Test
     public void testInscripcionPrimerDia() {
         //un participante se inscribe a un concurso el primer dia de abierta la inscripción
         //setup
-        var maria = new Participante(1, "maria", "23456789", "mperez@gmail.com");
-        LocalDateTime fechaInicio = LocalDateTime.of(2025, 2, 11, 12, 10, 5);
-        LocalDateTime fechaFin = LocalDateTime.of(2025, 2, 15, 12, 20, 1);
-        Concurso unConcurso = new Concurso(1, "concurso 1", fechaInicio, fechaFin);
 
         //ejercitación
-        unConcurso.nuevaInscripcion(maria, fechaInicio);
+        concurso.nuevaInscripcion(maria, fechaInicio);
 
         //verificación
-        assertTrue(unConcurso.participanteInscripto(maria));
-        assertEquals(1, unConcurso.cantidadInscriptos());
+        assertTrue(concurso.participanteInscripto(maria));
+        assertEquals(1, concurso.cantidadInscriptos());
         assertEquals(PUNTOS_GANADOS, maria.puntos());
-
     }
 
     @Test
     public void testInscripcionFueraDeRango(){
         //un participante se inscribe fuera de rango
         //setup
-        var maria = new Participante(1, "maria", "23456789", "mperez@gmail.com");
-        LocalDateTime fechaInicio = LocalDateTime.of(2025, 2, 11, 12, 10, 5);
-        LocalDateTime fechaFin = LocalDateTime.of(2025, 2, 15, 12, 20, 1);
         LocalDateTime fechaInscripcion = LocalDateTime.of(2025, 2, 16, 12, 20, 1);
-        Concurso unConcurso = new Concurso(1, "concurso 1", fechaInicio, fechaFin);
 
-    //opcion 2 separar exercise de verify
-        RuntimeException exception = null;
-        try {
-            unConcurso.nuevaInscripcion(maria, fechaInscripcion);
-        } catch (RuntimeException excepcion) {
-            exception = excepcion;
-        }
+        //ejercitación y verificación
+        RuntimeException exception = assertThrows(RuntimeException.class, ()->{
+                    concurso.nuevaInscripcion(maria, fechaInscripcion);
+                });
+
         //Verify
-        //verificar que al inscribir a un participante se invoque al m enviarCorreo
-        assertEquals(0, unConcurso.cantidadInscriptos());
+        assertEquals(0, concurso.cantidadInscriptos());
         assertNotNull(exception);
         assertEquals(Concurso.FECHA_FUERA_DE_RANGO, exception.getMessage());
-        assertFalse(unConcurso.participanteInscripto(maria));
-
+        assertFalse(concurso.participanteInscripto(maria));
     }
     @Test
     public void testFechaNula() {
